b50b09fa627c2cd0c516f561dd02b429a1df1c50
Casey Weed <me@caseyweed.net>
2014-04-09 14:44:26 -0400

    added more metrics, updated template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3 as sql
 DATABASE = 'idle.db'
 app = Flask(__name__)
 
+# SQL & DB 
 def connect_db():
 	return sql.connect(DATABASE)
 
@@ -25,32 +26,41 @@ def query_db(query, args=(), one=False):
 	cur.close()
 	return (rv[0] if rv else None) if one else rv
 
+# ERROR HANDLING
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('error/404.html', error=e), 404
 
-@app.route('/user', methods=['GET'])
+# API
+@app.route('/api/', methods=['GET'])
 def all_users():
 	data = query_db('select * from users order by points desc')
 	if data is None:
-		return 'No users available.'
+		return jsonify({})
 	users = []
+	total = 0
 	for user in data:
 		users.append({'name': user[0], 'points': user[1]})
-	return jsonify(users=users)
+		total = total + user[1]
+	return jsonify(users=users, total=total, registered=len(data))
 
-@app.route('/user/<username>', methods=['GET'])
+@app.route('/api/user/<username>', methods=['GET'])
 def get_user(username):
-	data = query_db('select * from users where name = ?', (username, ), True)
+	data = query_db('select name, points from users where name = ?', (username, ), True)
 	if data is None:
-		abort(404)
-	user = {'name': data[0], 'points': data[1]}
-	return jsonify(user=user)
+		return jsonify({})
+	return jsonify({'name': data[0], 'points': data[1]})
 
+# ROUTING
 @app.route('/')
 def default():
 	data = query_db('select * from users order by points desc')
-	return render_template('leaderboard.html', data=data)
+	last = data[-1][0]
+	reg = len(data)
+	time = 0
+	for user in data:
+		time += user[1]
+	return render_template('leaderboard.html', data=(data, reg, time, last))
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
